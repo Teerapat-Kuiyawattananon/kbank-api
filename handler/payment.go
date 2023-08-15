1a72501969a5a010a2bfb659c42ff35ff597f5ec
Teerapat Kuiyawattananon <teerapat.ku@ku.th>
2023-08-15 22:32:15 +0700

    fix payment

--- handler/payment.go
+++ handler/payment.go
@@ -51,13 +51,25 @@ func paymentConfirm(input model.PaymentRequest) (string, string) {
 	billDetailRepo := repo.NewBillDetailRepository(clientDB)
 
 	// string to int
-	ref2_id, _ := strconv.Atoi(input.Reference2)
+	ref2_id, err := strconv.Atoi(input.Reference2)
+	if err != nil {
+		return "0001", "Invalid Payment reference number"
+	}
+	tranAmount, err := strconv.ParseFloat(input.TranAmount, 64)
+	if err != nil {
+		return "0004", "Invalid payment amount"
+	}
 	bill := billDetailRepo.GetBillDetailByRef2(ref2_id)
 	// if (bill.Status == "waiting") {
 	// 	bill.Update().SetStatus("paid").ExecX(context.Background())
 	// 	return "0000", "Success"
 	// }
-
+	if (bill == nil) {
+		return "9001", "Unauthorized"
+	}
+	if (tranAmount != bill.TranAmount) {
+		return "0004", "Invalid payment amount"
+	}
 	if (bill.Status == "unavailable") {
 		return "1000", "Other Merchant Error"
 	}
@@ -67,6 +79,9 @@ func paymentConfirm(input model.PaymentRequest) (string, string) {
 	}
 
 	// Waiting for payment
-	bill.Update().SetStatus("paid").ExecX(context.Background())
+	bill.Update().SetStatus("paid").
+					SetChannelCode(input.ChannelCode).
+					SetSenderBankCode(input.SenderBankCode).
+					ExecX(context.Background())
 	return "0000", "Success"
 }
